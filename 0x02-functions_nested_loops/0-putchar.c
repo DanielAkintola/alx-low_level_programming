571a28ccc5c0bc8526279c1f12128d0c16bfac66
DanielAkintola <Akintola Daniel>
2022-10-18 04:15:11 -0700

    recent

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "_putchar.h"
 
 /**
  * main - Entry point
@@ -6,17 +7,18 @@
  * Return: Always returns 0
  *
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
